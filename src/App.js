a7a35c7759623238e230a062710857b522418b6a
Sergiorq1 <sergiorq12016@gmail.com>
2023-01-25 12:31:29 -0700

    added functionality to checkboxes and text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [name, setName] = useState("")
+  const [peperoni, setPeperoni] = useState(false)
+  const [sausage, setSausage] = useState(false)
+  const [peppers, setPeppers] = useState(false)
+  const [onions, setOnions] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Head'>
+        <h1>Pizza Order</h1>
+      </div>
+      <div className="split left">
+        <div className='centered'>
+          <input
+            type = "text"
+            label = "Name"
+            placeholder='Name'
+            onChange={e => setName(e.target.value)}
+          />
+          <label>
+            <input 
+              type="checkbox"
+              id="peperoni"
+              
+              checked={peperoni}
+              onChange={() => setPeperoni(!peperoni)}
+            />
+            Peperoni
+            <input 
+              type="checkbox"
+              id="sausage"
+              checked={sausage}
+              onChange={() => setSausage(!sausage)}
+            />
+            Sausage
+            <input 
+              type="checkbox"
+              id="peppers"
+              checked={peppers}
+              onChange={() => setPeppers(!peppers)}
+            />
+            Peppers
+            <input 
+              type="checkbox"
+              id="onions"
+              checked={onions}
+              onChange={() => setOnions(!onions)}
+            />
+            Onions
+          </label>
+          <button>Submit</button>
+        </div>
+      </div>
+      <div className='split right'>
+        <div className='centered'>
+          <h1>Your order</h1>
+          {name && <div>{name}</div>}
+          {peperoni && <div>Pepperoni</div>}
+          {sausage && <div>Sausage</div>}
+          {peppers && <div>Peppers</div>}
+          {onions && <div>Onions</div>}
+        </div>
+      </div>
     </div>
+
   );
 }
 
